27eb85eba4cd7d8e3670e3ca56f66546599ae43b
MUHAMMMMED <amer66965@gmail.com>
2025-01-07 01:52:42 +0200

    Update SESSION

--- frontend/src/components/config.js
+++ frontend/src/components/config.js
@@ -1,6 +1,6 @@
 const Config = {
-  baseURL: 'http://127.0.0.1:8000',
-  MEDIA_URL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://smartcardnfc.com',
+  MEDIA_URL: 'https://smartcardnfc.com',
 };
 
 
